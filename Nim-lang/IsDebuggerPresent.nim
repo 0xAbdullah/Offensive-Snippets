241205b3558862c008b72d03cb7961864923693d
Abdullah AlZahrani <29471278+0xAbdullah@users.noreply.github.com>
2023-05-11 13:48:20 +0300

    Update IsDebuggerPresent.nim

--- Nim-lang/IsDebuggerPresent.nim
+++ Nim-lang/IsDebuggerPresent.nim
@@ -8,8 +8,8 @@ type
 proc MessageBox*(hWnd: HWND, lpText: LPCSTR, lpCaption: LPCSTR, uType: UINT): int32 
   {.discardable, stdcall, dynlib: "user32", importc: "MessageBoxA".}
 
-# https://learn.microsoft.com/en-us/windows/win32/api/debugapi/nf-debugapi-isdebuggerpresent
-#[
+#[ 
+https://learn.microsoft.com/en-us/windows/win32/api/debugapi/nf-debugapi-isdebuggerpresent
 This code uses the IsDebuggerPresent function to check whether a debugger is currently attached to the program.
 ]#
 proc IsDebuggerPresent*(): bool 
